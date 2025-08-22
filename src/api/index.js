38b6c92f6ee78e925716d9286b4cc0aa0e210a9d
azimjon-95 <>
2025-08-22 10:50:53 +0500

    Api

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const mainURL = axios.create({
-  baseURL: "http://localhost:8040/api",
-  // baseURL: "https://medme-b-test.medme.uz/api",
+  // baseURL: "http://localhost:8040/api",
+  baseURL: "https://afzal.richman.uz/api",
 });
 
 export default mainURL;
