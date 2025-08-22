de7ef491c4208079d5320322bafaa4a0163d6b85
azimjon-95 <>
2025-08-22 11:09:25 +0500

    Api one

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 
-const SOCKET_URL = `http://localhost:8040`;
-// const SOCKET_URL = `https://medme-b-test.medme.uz/`;
+// const SOCKET_URL = `http://localhost:8040`;
+const SOCKET_URL = `https://afzal.richman.uz`;
 const headers = { transports: ["websocket"] };
 const socket = io(SOCKET_URL, headers);
 
